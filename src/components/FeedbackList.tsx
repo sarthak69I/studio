a4bd4cd33e00071fa5e3ee7c82ac3b8b889f9d09
sarthak69I <jzjha215@gmail.com>
2025-06-17 11:29:06 +0530

    Update FeedbackList.tsx

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -325,7 +325,7 @@ export default function FeedbackList() {
               <User className="mr-2 h-5 w-5 text-primary" /> Add Your Name 
             </DialogTitle>
             <DialogDescription className="pt-2">
-              ".
+              
             </DialogDescription>
           </DialogHeader>
           <div className="py-4 space-y-4">
