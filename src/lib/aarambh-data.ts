c281bd2ddbdb0d903f16025669280043c94b8fbf
sarthak69I <jzjha215@gmail.com>
2025-06-28 18:59:52 +0530

    Update aarambh-data.ts

--- src/lib/aarambh-data.ts
+++ src/lib/aarambh-data.ts
@@ -197,8 +197,8 @@ export const aarambhCourseContent: CourseContentMap = {
       name: 'Human eye',
       lectures: [
         { id: 'Human eye L1', title: 'Human eye L1', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/217279667485356350.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4420694/175094118357238929414/index_4.m3u8' },
-        { id: 'Human eye L2', title: 'Human eye L2', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4426247/175102765297458929414/index_4.m3u8' },
-        
+        { id: 'Human eye L2', title: 'Human eye L2', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/71000202971201150.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4426247/175102765297458929414/index_4.m3u8' },
+        { id: 'Human eye L3', title: 'Human eye L3', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4427347/175111381865158929414/index_4.m3u8' },
       ]       
     }
   ],
