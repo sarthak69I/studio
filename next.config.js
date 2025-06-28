db7f157a017544356f56b99f6eb17a346115d88b
Jzjha <jzjha215@gmail.com>
2025-06-28 07:28:53 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ next.config.js
@@ -0,0 +1,50 @@
+
+/** @type {import('next').NextConfig} */
+const withPWAInit = require('next-pwa');
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dxixtlyravvxx.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+};
+
+module.exports = withPWA(nextConfig);
