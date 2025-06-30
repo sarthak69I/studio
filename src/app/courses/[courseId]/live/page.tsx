e3ed269f8a3e06e5cdabea7b164b1b1aed813512
sarthak69I <jzjha215@gmail.com>
2025-06-30 17:17:18 +0530

    Update page.tsx

--- src/app/courses/[courseId]/live/page.tsx
+++ src/app/courses/[courseId]/live/page.tsx
@@ -57,7 +57,7 @@ const courseLiveDetails: Record<string, LiveClassData> = {
     subtitle: "Interactive learning sessions for Class 9 Aarambh batch",
     class1Subject: "SCIENCE",
     class2Subject: "SST",
-    class1LiveStreamUrl: `${newStreamPlayerBaseUrl}${encodeURIComponent('')}`, 
+    class1LiveStreamUrl: `${newStreamPlayerBaseUrl}${encodeURIComponent('https://d133w6ldrek1er.cloudfront.net/out/v1/f15d86916b1f404baeb09967b920d86a/index_4.m3u8')}`, 
     class2LiveStreamUrl: `${newStreamPlayerBaseUrl}${encodeURIComponent('https://dga9kme080o0w.cloudfront.net/out/v1/90ab1354cfcd4c5b83cf78a87d96041e/index_4.m3u8')}`, 
     class1Visible: true,
     class2Visible: true,
