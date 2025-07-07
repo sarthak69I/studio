498b96a1426fe6233fbfe5c141b875a0ecf6c0f4
sarthak69I <jzjha215@gmail.com>
2025-07-07 20:06:39 +0530

    Update page.tsx

--- src/app/courses/[courseId]/live/page.tsx
+++ src/app/courses/[courseId]/live/page.tsx
@@ -58,7 +58,7 @@ const courseLiveDetails: Record<string, LiveClassData> = {
     class1Subject: "SCIENCE",
     class2Subject: "SST",
     class1LiveStreamUrl: `${newStreamPlayerBaseUrl}${encodeURIComponent('https://d133w6ldrek1er.cloudfront.net/out/v1/f15d86916b1f404baeb09967b920d86a/index_4.m3u8')}`, 
-    class2LiveStreamUrl: `${newStreamPlayerBaseUrl}${encodeURIComponent('https://d133w6ldrek1er.cloudfront.net/out/v1/c923787f6b3b40cbbfbdc51d2d76e51e/index_4.m3u8')}`, 
+    class2LiveStreamUrl: `${newStreamPlayerBaseUrl}${encodeURIComponent('https://dga9kme080o0w.cloudfront.net/out/v1/90ab1354cfcd4c5b83cf78a87d96041e/index_4.m3u8')}`, 
     class1Visible: true, 
     class2Visible: true,
   }
