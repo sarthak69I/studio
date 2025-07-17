19a777796805d7e6e76373f2e4b3de9f8c218844
sarthak69I <jzjha215@gmail.com>
2025-07-17 19:15:51 +0530

    Update aarambh9-data.ts

--- src/lib/aarambh9-data.ts
+++ src/lib/aarambh9-data.ts
@@ -94,7 +94,8 @@ export const aarambh9CourseContent: CourseContentMap = {
         { id: 'L2', title: 'Tissue L2', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/741669906953814700.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4476776/175197829846758929414/175197829846758929414_8929414.m3u8' },
         { id: 'L3', title: 'Tissue L3', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/551846601926949800.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4478171/175207564070148929414/index_4.m3u8' },
         { id: 'L4', title: 'Tissue L4', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/807777532215662500.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4481913/175249800035998929414/index_4.m3u8' },
-         { id: 'L5', title: 'Tissue L5', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4482890/175258462787808929414/index_4.m3u8' },
+         { id: 'L5', title: 'Tissue L5', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/199727414625477760.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4482890/175258462787808929414/index_4.m3u8' },
+        { id: 'L6', title: 'Tissue L6', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4484120/175268040413938929414/175268040413938929414_8929414.m3u8' },
        
       ]
     },
@@ -175,7 +176,9 @@ export const aarambh9CourseContent: CourseContentMap = {
         { id: 'L3', title: ' Socialism in Europe and the Russian Revolution 3', notesTitle: "L2 & 3", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/706384299026803200.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4477955/175206460824077125088/index_4.m3u8' },
         { id: 'L4', title: ' Socialism in Europe and the Russian Revolution 4', notesTitle: "L4 and 5", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/595903458031875600.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4479048/175215101442707125088/index_4.m3u8' },
          { id: 'L5', title: ' Socialism in Europe and the Russian Revolution 5', notesTitle: "L4 and 5", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/595903458031875600.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4482039/175250749992707125088/index_4.m3u8' },
-        { id: 'L6', title: ' Socialism in Europe and the Russian Revolution 6', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4483088/175259387081697125088/index_4.m3u8' },
+        { id: 'L6', title: ' Socialism in Europe and the Russian Revolution 6', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/159134390619897800.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4483088/175259387081697125088/index_4.m3u8' },
+        { id: 'L7', title: ' Socialism in Europe and the Russian Revolution 7', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/159134390619897800.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4483914/175266969453997125088/index_4.m3u8' },
+      { id: 'L8', title: ' Socialism in Europe and the Russian Revolution 8', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4484954/175275601327827125088/175275601327827125088_7125088.m3u8' },
       
       ]
     },
@@ -248,6 +251,7 @@ export const aarambh9CourseContent: CourseContentMap = {
       name: 'Triangles',
       lectures: [   
         { id: 'L1', title: 'Triangles L1', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/165917613057313250.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4480736/175232416413558752407/index_4.m3u8' },
+          { id: 'L2', title: 'Triangles L2', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: '' },
       
       ]
     },
