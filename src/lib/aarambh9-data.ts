fba93432297f2f0a51f6162cbce13350b939da00
sarthak69I <jzjha215@gmail.com>
2025-07-19 19:05:19 +0530

    Update aarambh9-data.ts

--- src/lib/aarambh9-data.ts
+++ src/lib/aarambh9-data.ts
@@ -347,7 +347,8 @@ export const aarambh9CourseContent: CourseContentMap = {
       name: 'Triangles',
       lectures: [   
         { id: 'L1', title: 'Triangles L1', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/165917613057313250.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4480736/175232416413558752407/index_4.m3u8' },
-          { id: 'L2', title: 'Triangles L2', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/4478842027453222.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4485929/175284331075788752407/175284331075788752407_8752407.m3u8' },
+          { id: 'L2', title: 'Triangles L2', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/4478842027453222.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4485929/175284331075788752407/index_4.m3u8' },
+                 { id: 'L3', title: 'Triangles L3', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4486805/175292922251078752407/index_4.m3u8' },
       
       ]
     },
