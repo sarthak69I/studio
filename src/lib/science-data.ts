43659ea329d856a588c8b15af3225982098474a7
sarthak69I <jzjha215@gmail.com>
2025-07-28 22:03:02 +0530

    Update science-data.ts

--- src/lib/science-data.ts
+++ src/lib/science-data.ts
@@ -153,7 +153,13 @@ const scienceOnlySubjects: CourseContentMap = {
      { id: 'L7', title: 'Chemical Bonding & Molecular Structure L7', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/505947132740186050.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4488132/175311417820352764802/index_5.m3u8' },
      { id: 'L8', title: 'Chemical Bonding & Molecular Structure L8', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/321103323884252800.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4489883/175319882424292764802/index_5.m3u8' },
      { id: 'L9', title: 'Chemical Bonding & Molecular Structure L9 Doubt Class', notesTitle: "", notesLink: `https://dxixtlyravvxx.cloudfront.net/540admin_v1/file_manager/pdf/226129233654398620.pdf`, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4491460/175327503930232764802/index_5.m3u8' },
-    
+     ]
+    },
+    {
+      name: 'Thermodynamics',
+      lectures: [
+        { id: 'L1', title: 'Thermodynamics L1', notesTitle: "", notesLink: ``, videoEmbedType: 'hls', videoEmbedUrl: 'https://d274dp7v20n4nf.cloudfront.net/file_library/videos/channel_vod_non_drm_hls/4501805/175371849259392764802/index_5.m3u8' },
+       
         
       ]
     },
